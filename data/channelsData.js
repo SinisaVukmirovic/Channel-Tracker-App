1ffffde65ac1035f8f890be3b6cc82e5fcf40133
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-05-14 23:51:27 +0200

    Added other channel data

--- data/channelsData.js
+++ data/channelsData.js
@@ -1,6 +1,14 @@
 const channels = [
     {
-        topics: ['all', 'coding']
+        topics: ['all', 'coding'],
+        image: 'https://yt3.googleusercontent.com/ytc/AIdro_mk2Ex-8sW03SBlBX7D1EC5skH0kv9rS3rU9IXq2I-q2Zg=s160-c-k-c0x00ffffff-no-rj',
+        name: 'Net Ninja',
+        info: 'Black-belt your web development skills. Over 2000 free programming tutorial videos',
+        details: {
+            subs: '1.44M subs',
+            vids: '2,346 videos'
+        },
+        link: 'https://www.youtube.com/@NetNinja/videos'
     },
     {
         topics: ['all', 'design']
