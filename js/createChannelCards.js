27e32368babeee180ccc8b46905b837779cce2fb
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-05-21 20:44:03 +0200

    Search functionality looks like working

--- js/createChannelCards.js
+++ js/createChannelCards.js
@@ -1,5 +1,8 @@
 import { channelsData } from '../data/channelsData.js';
 import { channelsWrapper } from './DOMelems.js';
+import filterBySearchTerm from './filterBySearchTerm.js';
+// 
+// import { searchInput } from './DOMelems.js';
 
 import populateTagsInChannel from './populateTagsInChannel.js';
 
@@ -72,4 +75,51 @@ export default function createChannelCards() {
 
     populateTagsInChannel(filteredChannelData);
 
+
+    // 
+    filterBySearchTerm(filteredChannelData);
+    // searchInput.addEventListener('input', () => {
+    //     const searchTerm = searchInput.value;
+    //     const searchedChannelName = filteredChannelData.filter(chan => chan.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    //     console.log(searchedChannelName)
+    // });
 }
+
+// function filterBySearchTerm(filteredChannelData) {
+//     searchInput.addEventListener('input', () => {
+//         const searchTerm = searchInput.value;
+//         const searchedChannelName = filteredChannelData.filter(chan => chan.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+//         channelsWrapper.innerHTML = '';
+
+//         searchedChannelName.forEach(chan => {
+//             const channel = document.createElement('div');
+
+//             channel.setAttribute('data-channel', '');
+//             channel.classList.add('channel');
+            
+//             channel.innerHTML = `
+//                 <img src=${chan.image} alt="Channel Image">
+//                 <div class="tags-name">
+//                     <div data-tags class="tags">
+//                     </div>
+//                     <h4>${chan.name}</h4>
+//                 </div>
+//                 <div class="info">
+//                     <p>${chan.info}</p>
+//                 </div>
+//                 <div class="subs-link">
+//                     <div class="details">
+//                         <p>${chan.details.subs}</p>
+//                         <p>${chan.details.vids}</p>
+//                     </div>
+//                     <a href=${chan.link} target="_blank">Visit Channel</a>
+//                 </div>
+//             `;
+
+//             channelsWrapper.append(channel);
+//         });
+        
+//         populateTagsInChannel(searchedChannelName);
+//     });
+// }
---
+++ js/filterBySearchTerm.js
@@ -0,0 +1,41 @@
+import { channelsWrapper, searchInput } from "./DOMelems.js";
+import populateTagsInChannel from './populateTagsInChannel.js';
+
+export default function filterBySearchTerm(filteredChannelData) {
+    searchInput.addEventListener('input', () => {
+        const searchTerm = searchInput.value;
+        const searchedChannelName = filteredChannelData.filter(chan => chan.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+        channelsWrapper.innerHTML = '';
+
+        searchedChannelName.forEach(chan => {
+            const channel = document.createElement('div');
+
+            channel.setAttribute('data-channel', '');
+            channel.classList.add('channel');
+            
+            channel.innerHTML = `
+                <img src=${chan.image} alt="Channel Image">
+                <div class="tags-name">
+                    <div data-tags class="tags">
+                    </div>
+                    <h4>${chan.name}</h4>
+                </div>
+                <div class="info">
+                    <p>${chan.info}</p>
+                </div>
+                <div class="subs-link">
+                    <div class="details">
+                        <p>${chan.details.subs}</p>
+                        <p>${chan.details.vids}</p>
+                    </div>
+                    <a href=${chan.link} target="_blank">Visit Channel</a>
+                </div>
+            `;
+
+            channelsWrapper.append(channel);
+        });
+        
+        populateTagsInChannel(searchedChannelName);
+    });
+}
